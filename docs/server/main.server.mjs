4b2fa6a5ebe2e3843a409107d1e35d122aa4f6fe
jorgedevel0p <jorgemedelorte@gmail.com>
2025-06-10 00:20:13 -0400

    docss subido corregido

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SCJANAOC.mjs";import"./chunk-YLB36HKK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JZKSOBA4.mjs";import"./chunk-YLB36HKK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
