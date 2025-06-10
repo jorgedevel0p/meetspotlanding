67d63b9c8a1927d2bf2e001b768af461d60d530d
jorgedevel0p <jorgemedelorte@gmail.com>
2025-06-10 01:08:11 -0400

    se suben el termino y condiciones
    se da tambien un estilo a la pag

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TermsPageComponent } from '../terms-page/terms-page.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet,TermsPageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
