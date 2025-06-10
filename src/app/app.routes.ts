1faf0ee1d81a77d0253ac01f0ef4d19107c0d363
jorgedevel0p <jorgemedelorte@gmail.com>
2025-06-09 22:44:31 -0400

    se crea y sube la pag de meetspot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TermsPageComponent } from './pages/terms-page/terms-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'terms', component: TermsPageComponent },
+    { path: '**', redirectTo: '' },
+
+];
---
+++ src/app/pages/terms-page/terms-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './terms-page.component.html',
+  styleUrl: './terms-page.component.css'
+})
+export class TermsPageComponent {
+
+}
